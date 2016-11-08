30af6b7eed6e32b73379223535a9ec4550971917
lav <lav@LOVCHIKOV>
2016-11-08 19:43:08 +0300

    пример 2

---
+++ JR02/JR02/GetBestProductIdsQuery.sql
@@ -0,0 +1,9 @@
+﻿select ProductId, count(CustomerId)
+from
+(
+   select ProductId, CustomerId, 
+             ROW_NUMBER() over(partition by CustomerId order by DateCreated) rowNum
+   from Sales
+) z
+where rowNum = 1
+group by z.ProductId
